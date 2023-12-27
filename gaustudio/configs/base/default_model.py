98a1edd8678e618d7b2ad40e5875b2677ac2657e
hugoycj <576550489@qq.com>
2023-12-27 16:14:41 +0800

    Remove unused code

--- gaustudio/configs/base/default_model.py
+++
@@ -1,7 +0,0 @@
-sh_degree = 3
-_source_path = ""
-_model_path = ""
-_images = "images"
-_resolution = -1
-_white_background = False
-data_device = "cuda"
--- gaustudio/configs/vanilla.py
+++
@@ -1,2 +0,0 @@
-_base_ = ["base/default_dataset.py", "base/default_model.py", 
-          "base/default_optimization.py", "base/default_pipeline.py"]

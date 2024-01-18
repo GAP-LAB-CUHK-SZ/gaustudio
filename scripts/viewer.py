6b4dd2c5b45bdebda0a3ef0ebee7fa4b26c45eb2
hugoycj <576550489@qq.com>
2024-01-18 21:22:36 +0800

    Add renderer

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -11,9 +11,7 @@ import numpy as np
 import viser
 import viser.transforms as vtf
 import torch
-from utils.viewer import ClientThread
 
-import torch
 import os
 from tqdm import tqdm
 from os import makedirs
@@ -29,6 +27,136 @@ DROPDOWN_USE_DIRECT_APPEARANCE_EMBEDDING_VALUE = "@Direct"
 def searchForMaxIteration(folder):
     saved_iters = [int(fname.split("_")[-1]) for fname in os.listdir(folder)]
     return max(saved_iters)
+
+import time
+import threading
+import traceback
+from gaustudio.datasets import Camera
+
+class ClientThread(threading.Thread):
+    def __init__(self, viewer, client: viser.ClientHandle):
+        super().__init__()
+        self.viewer = viewer
+        self.client = client
+
+        self.render_trigger = threading.Event()
+
+        self.last_move_time = 0
+
+        self.last_camera = None  # store camera information
+
+        self.state = "low"  # low or high render resolution
+
+        self.stop_client = False  # whether stop this thread
+
+        client.camera.up_direction = viewer.up_direction
+
+        @client.camera.on_update
+        def _(cam: viser.CameraHandle) -> None:
+            with self.client.atomic():
+                self.last_camera = cam
+                self.state = "low"  # switch to low resolution mode when a new camera received
+                self.render_trigger.set()
+
+    def render_and_send(self):
+        with self.client.atomic():
+            cam = self.last_camera
+
+            self.last_move_time = time.time()
+
+            # get camera pose
+            R = vtf.SO3(wxyz=self.client.camera.wxyz)
+            R = R @ vtf.SO3.from_x_radians(np.pi)
+            R = R.as_matrix()
+            pos = self.client.camera.position
+            c2w = np.eye(4)
+            c2w[:3, :3] = R
+            c2w[:3, 3] = pos
+
+            c2w = np.matmul(self.viewer.camera_transform, c2w)
+
+            # change from OpenGL/Blender camera axes (Y up, Z back) to COLMAP (Y down, Z forward)
+            c2w[:3, 1:3] *= -1
+
+            # get the world-to-camera transform and set R, T
+            w2c = np.linalg.inv(c2w)
+            R = w2c[:3, :3]
+            T = w2c[:3, 3]
+
+            # calculate resolution
+            aspect_ratio = cam.aspect
+            max_res, jpeg_quality = self.get_render_options()
+            image_height = max_res
+            image_width = int(image_height * aspect_ratio)
+            if image_width > max_res:
+                image_width = max_res
+                image_height = int(image_width / aspect_ratio)
+
+            # construct camera
+            camera = Camera(
+                R=R,
+                T=T,
+                FoVx=cam.fov,
+                FoVy=cam.fov,
+                image_width=torch.tensor([image_width], dtype=torch.int),
+                image_height=torch.tensor([image_height], dtype=torch.int),
+                data_device=self.viewer.device
+            )
+
+            with torch.no_grad():
+                render_pkg = self.viewer.renderer.render(camera, self.viewer.gaussians)
+                image = render_pkg["render"]
+                image = torch.clamp(image, max=1.)
+                image = torch.permute(image, (1, 2, 0))
+                self.client.set_background_image(
+                    image.cpu().numpy(),
+                    format=self.viewer.image_format,
+                    jpeg_quality=jpeg_quality,
+                )
+
+    def run(self):
+        while True:
+            trigger_wait_return = self.render_trigger.wait(0.2)  # TODO: avoid wasting CPU
+            # stop client thread?
+            if self.stop_client is True:
+                break
+            if not trigger_wait_return:
+                # skip if camera is none
+                if self.last_camera is None:
+                    continue
+
+                # if we haven't received a trigger in a while, switch to high resolution
+                if self.state == "low":
+                    self.state = "high"  # switch to high resolution mode
+                else:
+                    continue  # skip if already in high resolution mode
+
+            self.render_trigger.clear()
+
+            try:
+                self.render_and_send()
+            except Exception as err:
+                print("error occurred when rendering for client")
+                traceback.print_exc()
+                break
+
+        self._destroy()
+
+    def get_render_options(self):
+        if self.state == "low":
+            return self.viewer.max_res_when_moving.value, int(self.viewer.jpeg_quality_when_moving.value)
+        return self.viewer.max_res_when_static.value, int(self.viewer.jpeg_quality_when_static.value)
+
+    def stop(self):
+        self.stop_client = True
+        # self.render_trigger.set()  # TODO: potential thread leakage?
+
+    def _destroy(self):
+        print("client thread #{} destroyed".format(self.client.client_id))
+        self.viewer = None
+        self.renderer = None
+        self.client = None
+        self.last_camera = None
 
 class Viewer:
     def __init__(
@@ -247,10 +375,10 @@ class Viewer:
         """
 
         # create client thread
-        # client_thread = ClientThread(self, self.viewer_renderer, client)
-        # client_thread.start()
+        client_thread = ClientThread(self, client)
+        client_thread.start()
         # store this thread
-        # self.clients[client.client_id] = client_thread
+        self.clients[client.client_id] = client_thread
 
     def _handle_client_disconnect(self, client: viser.ClientHandle):
         """

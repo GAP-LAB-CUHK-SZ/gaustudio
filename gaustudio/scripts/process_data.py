5291f53eba077331a4caf92ebd07103b27f0b043
hugoycj <576550489@qq.com>
2024-04-14 01:36:49 +0800

    Update process_data scripts

--- gaustudio/scripts/process_data.py
+++ gaustudio/scripts/process_data.py
@@ -1,12 +1,12 @@
 import click
 
 @click.command()
+@click.option('--dataset', '-d', type=str, default='colmap', help='Dataset name (polycam, mvsnet, nerf, scannet, waymo)')
 @click.option('--source_path', '-s', help='path to the dataset')
 @click.option('--output-dir', '-o', help='path to the output dir')
-@click.option('--dataset', type=str, default='colmap', help='Dataset name (polycam, mvsnet, nerf, neus, scannet)')
-@click.option('--initializer', type=str, help='Initializer name (colmap, loftr, midas, mvsnet, dust3r, pixelsplat)')
+@click.option('--initializer',  default='colmap', type=str, help='Initializer name (colmap, loftr, dust3r, mvsplat, midas)')
 @click.option('--overwrite', help='Overwrite existing files', is_flag=True)
-def main(source_path, output_dir, dataset, initializer, overwrite):
+def main(dataset, source_path, output_dir, initializer, overwrite):
     from gaustudio import datasets
     dataset_config = { "name":dataset, "source_path": source_path, "images":"images", "resolution":-1, "data_device":"cuda", "eval": False}
     dataset = datasets.make(dataset_config)

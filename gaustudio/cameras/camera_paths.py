f7049c01db123cce172df68ce520760188c77eeb
hugoycj <576550489@qq.com>
2024-04-25 21:25:11 +0800

    Add rotation smoothing

--- gaustudio/cameras/camera_paths.py
+++ gaustudio/cameras/camera_paths.py
@@ -2,7 +2,7 @@ import json
 import copy 
 import torch
 from gaustudio.utils.cameras_utils import JSON_to_camera
-from gaustudio.utils.pose_utils import get_interpolated_poses
+from gaustudio.utils.pose_utils import get_interpolated_poses, quaternion_from_matrix, quaternion_matrix
 
 def get_path_from_json(json_path):
     print("Loading camera data from {}".format(json_path))
@@ -31,18 +31,36 @@ def upsample_cameras(cameras, steps_per_transition=30):
             total_idx+=1
     return new_cameras
 
-from scipy.signal import medfilt
-def smoothen_cameras(cameras, kernel_size = 9):
+from scipy.signal import savgol_filter
+import numpy as np
+import numpy.linalg as la
+def unwrap_quaternions(qvecs):
+    qvecs_unwrapped = np.zeros_like(qvecs)
+    qvecs_unwrapped[0] = qvecs[0]
+    for i in range(1, qvecs.shape[0]):
+        dot = np.clip(np.sum(qvecs_unwrapped[i-1] * qvecs[i]), -1.0, 1.0)
+        qvecs_unwrapped[i] = (qvecs[i] if dot > 0 else -qvecs[i])
+    return qvecs_unwrapped
+
+def smoothen_cameras(cameras, window_length=9, polyorder=2):
     new_cameras = []
     total_idx = 0
-    translates = torch.stack([camera.extrinsics[:3,3] for camera in cameras]).cpu().numpy()
+    translates = torch.stack([camera.extrinsics[:3, 3] for camera in cameras]).cpu().numpy()
+    qvecs = np.stack([quaternion_from_matrix(camera.extrinsics[:3, :3].cpu().numpy()) for camera in cameras])
+    qvecs = unwrap_quaternions(qvecs)
     for dim in range(translates.shape[1]):
-        translates[:, dim] = medfilt(translates[:, dim], kernel_size)
+        translates[:, dim] = savgol_filter(translates[:, dim], window_length, polyorder)
     
-    for camera, smooth_translate in zip(cameras, translates):
+    for dim in range(qvecs.shape[1]):
+        qvecs[:, dim] = savgol_filter(qvecs[:, dim], window_length, polyorder)
+
+    for camera, smooth_translate, smooth_qvec in zip(cameras, translates, qvecs):
+        smooth_qvec /= la.norm(smooth_qvec)  # Normalize quaternion
         camera_new = copy.deepcopy(camera)
-        camera_new.extrinsics[:3, 3] = torch.from_numpy(smooth_translate).cuda()
+        updated_extrinsics = quaternion_matrix(smooth_qvec)
+        updated_extrinsics[:3, 3] = smooth_translate
+        camera_new.extrinsics = updated_extrinsics
         new_cameras.append(camera_new)
-        total_idx+=1
+        total_idx += 1
+
     return new_cameras
-        

dc9281a886031f87e6e9bf881de30eca49a9dc1d
hugoycj <576550489@qq.com>
2024-07-25 03:02:55 +0800

    feat: Add resolution and rename initializer option for gs-process
    - Add resolution option to CLI with default value of 1
    - Rename CLI option from 'initializer' to 'init'
    - Update corresponding function parameters and variable names

--- gaustudio/scripts/process_data.py
+++ gaustudio/scripts/process_data.py
@@ -5,10 +5,12 @@ from typing import Optional
 @click.option('--dataset', '-d', type=str, default='colmap', help='Dataset name (polycam, mvsnet, nerf, scannet, waymo)')
 @click.option('--source_path', '-s', required=True, help='Path to the dataset')
 @click.option('--output_dir', '-o', required=True, help='Path to the output directory')
-@click.option('--initializer', default='colmap', type=str, help='Initializer name (colmap, loftr, dust3r, mvsplat, midas)')
+@click.option('--init', default='colmap', type=str, help='Initializer name (colmap, loftr, dust3r, mvsplat, midas)')
 @click.option('--overwrite', help='Overwrite existing files', is_flag=True)
 @click.option('--w_mask', '-w', is_flag=True, help='Use mask')
-def main(dataset: str, source_path: Optional[str], output_dir: Optional[str], initializer: str, overwrite: bool, w_mask: bool) -> None:
+@click.option('--resolution', '-r', default=1, type=int, help='Resolution')
+def main(dataset: str, source_path: Optional[str], output_dir: Optional[str], 
+         init: str, overwrite: bool, w_mask: bool, resolution: int) -> None:
     """
     Main function to run the pipeline.
 
@@ -16,7 +18,7 @@ def main(dataset: str, source_path: Optional[str], output_dir: Optional[str], in
         dataset (str): Name of the dataset.
         source_path (Optional[str]): Path to the dataset.
         output_dir (Optional[str]): Path to the output directory.
-        initializer (str): Name of the initializer.
+        init (str): Name of the initializer.
         overwrite (bool): Whether to overwrite existing files.
         with_mask (bool): Whether to use mask.
     """
@@ -27,15 +29,14 @@ def main(dataset: str, source_path: Optional[str], output_dir: Optional[str], in
     dataset_config = {
         "name": dataset,
         "source_path": source_path,
-        "images": "images",
         "w_mask": w_mask,
         "camera_number": 1,
-        "resolution": 1,
+        "resolution": resolution,
     }
 
     dataset_instance = datasets.make(dataset_config)
     pcd = models.make("general_pcd")
-    initializer_config = {"name": initializer, "workspace_dir": output_dir}
+    initializer_config = {"name": init, "workspace_dir": output_dir}
     initializer_instance = initializers.make(initializer_config)
 
     initializer_instance(pcd, dataset_instance, overwrite=overwrite)

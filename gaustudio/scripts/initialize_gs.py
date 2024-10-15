aa4d88c2c1bb690669953dde484ca0af4685c895
hugoycj <576550489@qq.com>
2024-10-15 22:00:23 +0800

    feat: Add combined pcd output for gs-init

--- gaustudio/scripts/initialize_gs.py
+++ gaustudio/scripts/initialize_gs.py
@@ -11,8 +11,9 @@ from gaustudio.utils.misc import load_config
 @click.option('--w_mask', '-w', is_flag=True, help='Use mask')
 @click.option('--resolution', '-r', default=1, type=int, help='Resolution')
 @click.option('--model', '-m', help='path to model')
+@click.option('--pcd', type=click.Choice(['dust3r', 'combined']), default='dust3r', help='Point cloud to use: dust3r or combined (hloc+dust3r)')
 def main(dataset: str, source_path: Optional[str], output_dir: Optional[str], 
-        overwrite: bool, w_mask: bool, resolution: int, model: str) -> None:
+        overwrite: bool, w_mask: bool, resolution: int, model: str, pcd: bool) -> None:
     from gaustudio import datasets
     from gaustudio import models
     from gaustudio.pipelines import initializers
@@ -26,17 +27,21 @@ def main(dataset: str, source_path: Optional[str], output_dir: Optional[str],
 
     dataset = datasets.make(dataset_config)
     dataset.all_cameras = [_camera.downsample_scale(resolution) for _camera in dataset.all_cameras]
-    pcd = models.make("general_pcd")
-    
-    hloc_initializers = initializers.make({"name": 'hloc', "workspace_dir": output_dir})
-    hloc_initializers(pcd, dataset, overwrite=overwrite)
-
+    dust3r_pcd = models.make("general_pcd")
     initializer_config = {"name": 'dust3r', "workspace_dir": os.path.join(output_dir, 'data'), 
                           "model_path":model}
     initializer_instance = initializers.make(initializer_config)
-    initializer_instance(pcd, dataset, overwrite=overwrite)
+    initializer_instance(dust3r_pcd, dataset, overwrite=overwrite)
     
-    pcd.export(os.path.join(output_dir, 'sparse', '0', 'points3D.ply'))
+    if pcd == 'combined':
+        hloc_pcd = models.make("general_pcd")
+        hloc_initializers = initializers.make({"name": 'hloc', "workspace_dir": output_dir})
+        hloc_initializers(hloc_pcd, dataset, overwrite=overwrite)
+        final_pcd = hloc_pcd + dust3r_pcd
+    else:
+        final_pcd = dust3r_pcd
+    
+    final_pcd.export(os.path.join(output_dir, 'sparse', '0', 'points3D.ply'))
 
 if __name__ == '__main__':
     main()
